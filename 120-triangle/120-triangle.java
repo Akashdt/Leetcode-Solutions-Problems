21a407b6f3d631a9589be7d3b649c209af08643e
Akash Dutta <64660695+Akashdt@users.noreply.github.com>
2022-06-13 23:31:11 +0530

    Time: 4 ms (61.51%), Space: 44.8 MB (13.58%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,24 +1,12 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) 
-    {
-        traverse(triangle, triangle.size() - 1);
-        return triangle.get(0).get(0);
-    }
-    
-    private void traverse(List<List<Integer>> triangle, int level) 
-    {
-        if (level == 0) 
-        {
-            return;
-        }
-        
-        List<Integer> row = triangle.get(level);
-        List<Integer> upperRow = triangle.get(level - 1);
-        for (int i = 0; i < upperRow.size(); i++) 
-        {
-            upperRow.set(i, upperRow.get(i) + Math.min(row.get(i),
-                                                       row.get(i+1)));
-        }
-        traverse(triangle, level - 1);
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+	int[][] dp = new int[n+1][n+1];
+
+	for (int level=n-1; level>=0; level--)
+		for (int i=0; i<=level; i++)
+			dp[level][i] = triangle.get(level).get(i) + Math.min(dp[level+1][i], dp[level+1][i+1]);
+
+	return dp[0][0];
     }
 }
