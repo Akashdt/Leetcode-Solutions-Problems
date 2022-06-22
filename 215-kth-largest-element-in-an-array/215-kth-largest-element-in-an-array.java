506f57ffd508444eb6e77a9c9a31b8236458918e
Akash Dutta <64660695+Akashdt@users.noreply.github.com>
2022-06-22 22:02:42 +0530

    Time: 10 ms (29.38%), Space: 45.1 MB (31.00%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,47 +1,15 @@
-class Solution 
-{
-    private final Random r = new Random();
-        
-    public int findKthLargest(int[] nums, int k) 
+class Solution {
+    public int findKthLargest(int[] nums, int k)
     {
-        int index = nums.length - k;
-        int lo = 0;
-        int hi = nums.length - 1;
-        while (lo < hi) 
+        Queue<Integer> minHeap = new PriorityQueue<>();
+        for (int i: nums) 
         {
-            int pivotIdx = partition(nums, lo, hi);
-            if (pivotIdx == index) {
-                break;
-            } else if (pivotIdx < index)
+            minHeap.add(i);
+            if (minHeap.size() > k)
             {
-                lo = pivotIdx + 1;
-            } else 
-            {
-                hi = pivotIdx - 1;
+                minHeap.remove();
             }
         }
-        return nums[index];
-    }
-    
-    private int partition(int[] nums, int lo, int hi) 
-    {
-        swap(nums, hi, lo + r.nextInt(hi - lo));
-        int pivot = nums[hi];
-        int j = lo;
-        for (int i = lo; i < hi; i++) 
-        {
-            if (nums[i] < pivot) {
-                swap(nums, i, j++);
-            }
-        }
-        swap(nums, hi, j);
-        return j;
-    }
-    
-    private void swap(int[] nums, int i, int j)
-    {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+        return minHeap.peek();
     }
 }
