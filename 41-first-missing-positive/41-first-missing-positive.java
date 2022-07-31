c2e4593fe6a56c726e113a953eca19135c8bebc1
Akash Dutta <64660695+Akashdt@users.noreply.github.com>
2022-07-31 13:38:00 +0530

    Time: 7 ms (32.82%), Space: 128 MB (34.67%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstMissingPositive(int[] v) {
+        Arrays.sort(v);
+        int flag = 1;
+        for (int i:v) 
+        {
+            if (flag == i) 
+            {
+                flag++;
+            } 
+        }
+        return flag;
+    }
+}
