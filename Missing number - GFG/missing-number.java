5734e97cc19138fd4cd547a5c12693e3dadadac4
Akash Dutta <64660695+Akashdt@users.noreply.github.com>
2022-10-12 12:37:54 +0530

    Added solution - LeetHub

---
+++ Missing number - GFG/missing-number.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n+5];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n-1; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            System.out.println(obj.missingNumber(a, n));
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    
+    public static int missingNumber(int A[], int N)
+    {
+        HashSet<Integer> h = new HashSet<Integer>();
+        for (int i:A)
+        {
+            h.add(i);
+            
+        }
+        for(int i=0;i<A.length;i++)
+        {
+            if(!h.contains(i))
+            {
+                return i;
+            }
+        }
+            return -1;
+        
+    }
+    
+}
