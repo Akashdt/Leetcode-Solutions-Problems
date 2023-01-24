16700886586a31d328925d14e8fc973584b1a7a4
Akash Dutta <64660695+Akashdt@users.noreply.github.com>
2023-01-24 20:50:44 +0530

    Added solution - LeetHub

--- Convert an array to reduced form - GFG/convert-an-array-to-reduced-form.java
+++ Convert an array to reduced form - GFG/convert-an-array-to-reduced-form.java
@@ -36,14 +36,13 @@ public class Main {
 
 class Solution {
     void convert(int[] arr, int n) {
+        
         int[] temp = arr.clone();
         Arrays.sort(temp);
-        Map<Integer,Integer> indexMap = new HashMap<>();
-        for(int index = 0;index <n;index++){
-            indexMap.put(temp[index],index);
-        }
-        for(int index = 0;index < n;index++){
-            arr[index] = indexMap.get(arr[index]);
-        }
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        
+        int val=0;
+        for(int num : temp) hm.put(num , val++);
+        for(int i=0;i<n;i++)    arr[i] = hm.get(arr[i]);
     }
 }
