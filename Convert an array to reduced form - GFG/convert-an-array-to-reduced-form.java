a2789b81e4650300dcdd151b883e91cb93c6d2d9
Akash Dutta <64660695+Akashdt@users.noreply.github.com>
2023-01-24 14:36:03 +0530

    Added solution - LeetHub

---
+++ Convert an array to reduced form - GFG/convert-an-array-to-reduced-form.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().convert(arr, n);
+            for (int i = 0; i < n; i++) {
+                out.print(arr[i] + " ");
+            }
+            out.println();
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    void convert(int[] arr, int n) {
+        int[] temp = arr.clone();
+        Arrays.sort(temp);
+        Map<Integer,Integer> indexMap = new HashMap<>();
+        for(int index = 0;index <n;index++){
+            indexMap.put(temp[index],index);
+        }
+        for(int index = 0;index < n;index++){
+            arr[index] = indexMap.get(arr[index]);
+        }
+    }
+}
