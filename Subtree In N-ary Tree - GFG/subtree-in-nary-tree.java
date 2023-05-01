d73f5edfcdd11fb4573120e8cecd892bb1156a6b
Akash Dutta <64660695+Akashdt@users.noreply.github.com>
2023-05-01 10:54:27 +0530

    Added solution - LeetHub

---
+++ Subtree In N-ary Tree - GFG/subtree-in-nary-tree.java
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-->0){
+            
+            String s[] = br.readLine().trim().split(" ");
+            N_ary_Tree tree = new N_ary_Tree();
+            Node curr = null;
+            Queue<Node> q = new LinkedList<>();
+            for(int i = 0; i < s.length; i++){
+                if(i == 0){
+                    curr = tree.add(Integer.parseInt(s[0]), null);
+                } else if(s[i].equals(" ")){
+                    continue;
+                } else if(!q.isEmpty() && s[i].equals("N")){
+                    curr = q.remove();
+                } else if(!s[i].equals("N")){
+                    q.add(tree.add(Integer.parseInt(s[i]), curr));
+                }
+            }
+            Solution soln = new Solution();
+            ot.println(soln.duplicateSubtreeNaryTree(tree.root));
+        }
+
+        ot.close();
+    }
+}
+class Node{
+    int data;
+    List<Node> children;
+    Node(int val){
+        this.data = val;
+        this.children = new ArrayList<>();
+    }
+}
+class N_ary_Tree{
+    Node root;
+    int size;
+    N_ary_Tree(){
+        this.size = 0;
+        this.root = null;
+    }
+    Node add(int key, Node parent){
+        Node node = new Node(key);
+        if(parent == null){
+            this.root = node;
+            this.size = 1;
+        } else{
+            parent.children.add(node);
+            this.size++;
+        }
+        return node;
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution{
+    HashMap<String,Integer> hm;
+    public int duplicateSubtreeNaryTree(Node root){
+        this.hm = new HashMap<>();
+        int dub = 0;
+        dub(root);
+        for(Map.Entry<String , Integer> en : hm.entrySet()){
+            if(en.getValue()>1){
+                dub+=1;
+            }
+        }
+        return dub;
+    }
+    private String dub(Node root){
+        String s = ""+root.data+"+";
+        
+        for(int i = 0 ; i < root.children.size() ; i++){
+            Node nd = root.children.get(i);
+            s+=i+"-"+dub(nd)+"-";
+        }
+        hm.merge(s,1,Integer::sum);
+        return "("+s+")";
+    }
+    
+}
