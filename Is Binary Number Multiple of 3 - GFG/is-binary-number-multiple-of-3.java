8e8ffcbe99b9069617a78fe0e6835744f113dcc3
Akash Dutta <64660695+Akashdt@users.noreply.github.com>
2023-06-30 10:28:16 +0530

    Added solution - LeetHub

---
+++ Is Binary Number Multiple of 3 - GFG/is-binary-number-multiple-of-3.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])throws IOException
+        {
+            BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter out=new PrintWriter(System.out);
+            int t = Integer.parseInt(in.readLine().trim());
+            while(t-->0)
+                {
+                    String s = in.readLine().trim();
+                    Solution ob = new Solution();
+                    out.println(ob.isDivisible(s));
+                }
+                out.close();
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int isDivisible(String s) {
+        java.math.BigInteger a = new java.math.BigInteger(s, 2);
+        if (a.mod(java.math.BigInteger.valueOf(3)).equals(java.math.BigInteger.ZERO)) {
+            return 1;
+        }
+        return 0;
+    }
+}
